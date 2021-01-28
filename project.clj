082b376599eff87aebb040798828dac4dfff3831
Shogo Ohta <athos0220@gmail.com>
2021-01-28 21:59:25 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject type-infer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure utility to inspect static types inferred by the Clojure compiler"
+  :url "https://github.com/athos/type-infer"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns type-infer.core})
+  :repl-options {:init-ns type-infer.core}
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.2"]]}})
