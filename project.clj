8ad0e3a23886e284c33e8be44c67f624ae37be78
Shogo Ohta <athos0220@gmail.com>
2021-01-29 23:50:54 +0900

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject type-infer "0.1.0-SNAPSHOT"
+(defproject type-infer "0.1.0"
   :description "A Clojure utility to inspect static types inferred by the Clojure compiler"
   :url "https://github.com/athos/type-infer"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
