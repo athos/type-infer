e9e287954039cc258154d7df84b6378c9e9e9a06
Shogo Ohta <athos0220@gmail.com>
2021-02-04 23:19:38 +0900

    Prepare for next development iteration (0.1.2-SNAPSHOT)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject type-infer "0.1.1"
+(defproject type-infer "0.1.2-SNAPSHOT"
   :description "A Clojure utility to inspect static types inferred by the Clojure compiler"
   :url "https://github.com/athos/type-infer"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
