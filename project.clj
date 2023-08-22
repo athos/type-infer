e005e50c6091184615d109ca4151c55e25104c58
Shogo Ohta <athos0220@gmail.com>
2023-08-23 01:21:21 +0900

    Drop project.clj

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject type-infer "0.1.2-SNAPSHOT"
-  :description "A Clojure utility to inspect static types inferred by the Clojure compiler"
-  :url "https://github.com/athos/type-infer"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :repl-options {:init-ns type-infer.core}
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.2"]]}})
